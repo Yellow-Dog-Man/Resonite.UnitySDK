c36e9ba31eacbb825a03ee61dd82f00d9dc2036a
Frooxius <tomas.mariancik@gmail.com>
2026-02-12 12:06:22 +0100

    feat: Implement initial bits of component conversion

---
+++ Assets/ResoniteSDK/ComponentConverters/Unity Core/Rendering/LightConverter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightConverter : ResoniteComponentConverter<Light>
+{
+    protected override void Cleanup()
+    {
+
+    }
+
+    protected override void UpdateConversion(Light target)
+    {
+
+    }
+}
---
+++ Assets/ResoniteSDK/Editor/ConverterRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+
+public static class ComponentConverterRepository
+{
+    public static Type TryGetConverter(Type unityType)
+    {
+        if (_converters.TryGetValue(unityType, out var converter))
+            return converter;
+
+        return null;
+    }
+
+    static Dictionary<Type, Type> _converters = new Dictionary<Type, Type>();
+
+    static ComponentConverterRepository()
+    {
+        foreach(var converter in TypeCache.GetTypesDerivedFrom<ResoniteComponentConverter>())
+        {
+            // Ignore abstract ones
+            if (converter.IsAbstract)
+                continue;
+
+            // Determine the type it converts from the generic base type
+            var unityType = GetUnityComponentType(converter);
+
+            _converters.Add(unityType, converter);
+        }
+    }
+
+    static Type GetUnityComponentType(Type type)
+    {
+        type = type.BaseType;
+
+        do
+        {
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(ResoniteComponentConverter<>))
+                return type.GetGenericArguments()[0];
+
+            type = type.BaseType;
+
+        } while (type.BaseType != null);
+
+        throw new Exception($"Could not determine conversion type for: {type}");
+    }
+}
