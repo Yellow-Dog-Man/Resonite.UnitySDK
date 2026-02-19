228f008e82d8d0b0c3870b2dd721d225a87e738c
Frooxius <tomas.mariancik@gmail.com>
2026-02-19 02:10:39 +0100

    feat: Add ReflectionProbe conversion support

--- Assets/ResoniteSDK/Utility/EnumConversionHelper.cs
+++ Assets/ResoniteSDK/Utility/EnumConversionHelper.cs
@@ -28,7 +28,33 @@ public static class EnumConversionHelper
             case FilterMode.Trilinear: return Renderite.Shared.TextureFilterMode.Trilinear;
 
             default:
-                throw new System.NotImplementedException($"Conversion not implemented for filter mode mode: {filterMode}");
+                throw new System.NotImplementedException($"Conversion not implemented for filter mode: {filterMode}");
+        }
+    }
+
+    public static Renderite.Shared.CameraClearMode ToResoniteLink(this UnityEngine.CameraClearFlags flags)
+    {
+        switch(flags)
+        {
+            case CameraClearFlags.Nothing: return Renderite.Shared.CameraClearMode.Nothing;
+            case CameraClearFlags.Depth: return Renderite.Shared.CameraClearMode.Depth;
+            case CameraClearFlags.Color: return Renderite.Shared.CameraClearMode.Color;
+            case CameraClearFlags.Skybox: return Renderite.Shared.CameraClearMode.Skybox;
+
+            default:
+                throw new System.NotImplementedException($"Conversion not implemented for camera flags: {flags}");
+        }
+    }
+
+    public static Renderite.Shared.ReflectionProbeClear ToResoniteLink(this UnityEngine.Rendering.ReflectionProbeClearFlags flags)
+    {
+        switch (flags)
+        {
+            case UnityEngine.Rendering.ReflectionProbeClearFlags.SolidColor: return Renderite.Shared.ReflectionProbeClear.Color;
+            case UnityEngine.Rendering.ReflectionProbeClearFlags.Skybox: return Renderite.Shared.ReflectionProbeClear.Skybox;
+
+            default:
+                throw new System.NotImplementedException($"Conversion not implemented for reflection probe clear flags: {flags}");
         }
     }
 }
