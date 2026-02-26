a1503c85096443cc99ef97225c43700ed6c6de00
Frooxius <tomas.mariancik@gmail.com>
2026-02-26 22:24:14 +0100

    feat: Initialize persistent/Enabled on components by default

---
+++ BindingsGenerator/Resonite.UnitySDK.Bindings/Extensions/ComponentBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FrooxEngine
+{
+    public partial class ComponentBase<C>
+    {
+        public ComponentBase()
+        {
+            // Some sane defaults
+            persistent = true;
+            Enabled = true;
+        }
+    }
+}
